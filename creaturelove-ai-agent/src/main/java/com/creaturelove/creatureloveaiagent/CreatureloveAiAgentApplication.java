49dccd974d44d7cd63eba477a14b02d4de9026c8
scinttt <scinttt@gmail.com>
2025-04-28 09:55:38 -0400

    Initialization

---
+++ creaturelove-ai-agent/src/main/java/com/creaturelove/creatureloveaiagent/CreatureloveAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.creaturelove.creatureloveaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatureloveAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatureloveAiAgentApplication.class, args);
+    }
+
+}
---
+++ creaturelove-ai-agent/src/test/java/com/creaturelove/creatureloveaiagent/CreatureloveAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creaturelove.creatureloveaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatureloveAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
